94d05a358147c1eead5ca28d17382b4591f7beca
Martijn Jacobs <martijn@fourdigits.nl>
2012-10-30 16:43:16 +0100

    Finally fixed rabbitmq management agent automatic restart when installing plugins

--- manifests/management.pp
+++ manifests/management.pp
@@ -2,5 +2,6 @@ class rabbitmqcluster::management {
     rabbitmq_plugin {'rabbitmq_management':
       ensure => present,
       provider => 'rabbitmqplugins',
+      notify => Service[rabbitmq-server]
     }
 }
