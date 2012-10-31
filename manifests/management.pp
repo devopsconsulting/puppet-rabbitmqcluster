add9f77e1722f3c45cc8447156a0f962e5f1c9b5
Martijn Jacobs <martijn@fourdigits.nl>
2012-10-31 15:37:08 +0100

    This will break the ramnode waiting, so we need a different soultion

--- manifests/management.pp
+++ manifests/management.pp
@@ -2,6 +2,5 @@ class rabbitmqcluster::management {
     rabbitmq_plugin {'rabbitmq_management':
       ensure => present,
       provider => 'rabbitmqplugins',
-      notify => Service["rabbitmq-server"]
     }
 }
